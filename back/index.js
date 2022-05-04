266b541d8ca73c7201d83c2971d8ea65e360604c
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-05-04 18:41:59 +0300

    mise en place de mangoose, revoir le routeur et les shemas de donnees

--- back/index.js
+++ back/index.js
@@ -8,6 +8,10 @@ const morgan = require("morgan");
 const favicon = require("serve-favicon");
 // router : fichier externe de gestion des routes
 const router = require("./routeur");
+// path : gestion des liens
+const path = require("path");
+// mongoose : gestion de la base de donne MongoDB
+const mongoose = require("mongoose");
 
 // express :
 app.listen(port, () => console.log("Nous sommes le port 3000"));
@@ -17,3 +21,26 @@ app.use(morgan("dev"));
 app.use(favicon("../front/src/favicon.ico"));
 // router : voir fichier router.js
 app.use("/", router);
+// connection avec la base de donnee (LIEN A RESEIGNER)
+mongoose
+  .connect("LIEN", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("la connextion a la base de donnee a reussie"))
+  .catch(() => console.log("la connexion a echouee !"));
+// Shema des donnees a stocker (A REVOIR)
+const shemaDesDonnees = mongoose.Schema({
+  nom: String,
+  sauce: String,
+  note: Number,
+});
+const modelDeDonnee = mongoose.model("sauce", shemaDesDonnees);
+// Gestion des donnees
+const sauce = modelDeDonnee
+  .find()
+  .exec()
+  .then((sauces) => {
+    console.log(sauces);
+  })
+  .catch();
